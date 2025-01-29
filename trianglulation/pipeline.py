09633064725cf542052b60678f5ea2a153532d41
robin <perperam@icatas.eu>
2025-01-30 00:42:22 +0100

    first working inpainting pipeline

--- trianglulation/pipeline.py
+++ trianglulation/pipeline.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import cv2
 import cv2.aruco as aruco
 import numpy as np
@@ -29,10 +31,17 @@ class Detector:
         fs.release()
 
 
-    def detect(self, gray_image):
+    def detect(self, image):
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow("Gray", gray_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
         detections = []
 
         corners, ids, rejected = self.detector.detectMarkers(gray_image)
+
 
         for i, id in enumerate(ids):
             marker_corners = corners[i][0]
@@ -47,42 +56,50 @@ class Detector:
 
             detections.append({
                 'id': marker_id,
+                'corners': marker_corners,
                 'rvec': rvec,
                 'tvec': tvec,
             })
 
+            mask = np.zeros(gray_image.shape[0:2], dtype=np.uint8)
+
+            print(type(gray_image))
+            print(marker_corners)
+            pts = marker_corners.astype(np.int32)
+
+            pts = np.array([[357, 336],
+                            [236, 325],
+                            [246, 205],
+                            [366, 215]], dtype=np.int32)
+
+            # https://docs.opencv.org/4.x/d6/d6e/group__imgproc__draw.html#ga8c69b68fab5f25e2223b6496aa60dad5
+
+            cv2.fillPoly(mask, [marker_corners.astype(np.int32)], (255,))
+
+            cv2.imshow("Mask", mask)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+            # cv::INPAINT_NS or cv::INPAINT_TELEA
+            inpainted_image = cv2.inpaint(image, mask, inpaintRadius=20, flags=cv2.INPAINT_NS)
+
+            cv2.imshow("Inpainted", inpainted_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
         return detections
-
-
-def calculate_positions(detections):
-    pass
-
-# def load_image():
-#     """
-#     load an image from source X<> and return it
-#     :return: image
-#     """
-#
-# def extract_marker_positions(gray_image):
-#     """
-#     extract from Image<> marker Number<...>
-#     :return:
-#     """
-#     corners, ids, rejected = detector.detectMarkers(gray)
-#
-#
-# def huhu():
-#     """
-#     :return:
-#     """
 
 
 if __name__ == "__main__":
     detector = Detector()
+    detector.load_camera()
 
+    image_path = Path("cap_00.jpg")
 
-    # image = load_image()
-    #
-    # markers = extract_marker_positions()
-    #
-    # position = calculate_position()
+    image = cv2.imread(str(image_path))
+
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    detector.detect(image)
